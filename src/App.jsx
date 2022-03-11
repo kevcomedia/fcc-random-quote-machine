9141d5ab66549847de6cc5678406273a9da80253
Kev Comedia <kevcomedia@gmail.com>
2022-03-11 23:59:23 +0800

    Implement random quotes

--- src/App.jsx
+++ src/App.jsx
@@ -6,42 +6,53 @@ const App = () => {
 
   useEffect(() => {
     setQuote(quoteService.newQuote())
-  })
+  }, [])
+
+  const handleNewQuoteClick = () => {
+    setQuote(quoteService.newQuote())
+  }
 
   return (
     <div
       id="quote-box"
       className="max-w-lg max-h-screen overflow-y-auto bg-white text-zinc-800 p-16 rounded-md drop-shadow-lg"
     >
-      <blockquote
-        id="text"
-        className="mb-4 text-xl leading-relaxed before:content-[open-quote] after:content-[close-quote]"
-      >
-        {quote.text}
-      </blockquote>
-      <p id="author" className="text-right mb-8">
-        &mdash; {quote.author}
-      </p>
+      {quote && (
+        <>
+          <blockquote
+            id="text"
+            className="mb-4 text-xl leading-relaxed before:content-[open-quote] after:content-[close-quote]"
+          >
+            {quote.text}
+          </blockquote>
+          <p id="author" className="text-right mb-8">
+            &mdash; {quote.author}
+          </p>
+        </>
+      )}
 
       <div className="flex">
         <button
           id="new-quote"
           className="px-3 py-2 mr-2 bg-red-500 hover:bg-red-600 focus:bg-red-600 focus:outline-none focus:ring focus:ring-red-400 text-white rounded-md border border-b-4 active:border-b border-red-900"
           type="button"
+          onClick={handleNewQuoteClick}
         >
           New quote
         </button>
-        <a
-          id="tweet-quote"
-          className="px-3 py-2 bg-red-500 hover:bg-red-600 focus:bg-red-600 focus:outline-none focus:ring focus:ring-red-400 text-white rounded-md border border-b-4 border-red-900"
-          href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(
-            `"${quote.text}" — ${quote.author}`
-          )}`}
-          target="_blank"
-          rel="noreferrer nofollow"
-        >
-          Tweet
-        </a>
+        {quote && (
+          <a
+            id="tweet-quote"
+            className="px-3 py-2 bg-red-500 hover:bg-red-600 focus:bg-red-600 focus:outline-none focus:ring focus:ring-red-400 text-white rounded-md border border-b-4 border-red-900"
+            href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(
+              `"${quote.text}" — ${quote.author}`
+            )}`}
+            target="_blank"
+            rel="noreferrer nofollow"
+          >
+            Tweet
+          </a>
+        )}
       </div>
     </div>
   )
--- src/services/quoteService.js
+++ src/services/quoteService.js
@@ -1,8 +1,41 @@
-const newQuote = () => {
-  return {
+const quotes = [
+  {
     text: "Design in art, is a recognition of the relation between various things, various elements in the creative flux. You can't invent a design. You recognize it, in the fourth dimension. That is, with your blood and your bones, as well as with your eyes.",
     author: 'D. H. Lawrence',
-  }
+  },
+  {
+    text: 'As a profession, graphic designers have been shamefully remiss or ineffective about plying their craft for social or political betterment.',
+    author: 'Steven Heller',
+  },
+  {
+    text: "Stop. I'm not going to take any more input until I've made something with what I got.",
+    author: 'Merlin Mann',
+  },
+  {
+    text: 'Nothing will crush your creative career like the weight of trying to make it a masterpiece. Leave the art to the stuff you make, let the career be a big messy experiment.',
+    author: 'Andy J. Pizza',
+  },
+  {
+    text: 'I am always doing that which I can not do, in order that I may learn how to do it.',
+    author: 'Pablo Picasso',
+  },
+  {
+    text: 'Usage is like oxygen for ideas.',
+    author: 'Matt Mullenweg',
+  },
+  {
+    text: 'If design becomes your life, you can be sure a lot of people will be making their living thanks to your designs.',
+    author: 'Andrés Contenti',
+  },
+  {
+    text: 'Everything we do communicates.',
+    author: 'Pete Episcopo',
+  },
+]
+
+const newQuote = () => {
+  const randomIndex = Math.floor(Math.random() * quotes.length)
+  return quotes[randomIndex]
 }
 
 export default { newQuote }
